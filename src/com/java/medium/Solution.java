bc23b0d09bbf59c480baaba71a823a2738290d9b
n-ndini7 <nan11041997dini@gmail.com>
2022-08-25 17:07:07 +0530

    Largest sum subArr - Kandane's algorithm

--- src/com/java/medium/Solution.java
+++ src/com/java/medium/Solution.java
@@ -2,29 +2,24 @@ package com.java.medium;
 
 public class Solution {
 	/*
-	 * Given an array of N integers arr[] where each element represents the max length of the
-	 *  jump that can be made forward from that element. Find the minimum number of jumps to reach 
-	 *  the end of the array (starting from the first element). If an element is 0,
-	 *   then you cannot move through that element.
-
-Note: Return -1 if you can't reach the end of the array.
-	 */
-	 static int minJumps(int[] arr){
-	        // your code here
-	        int jump=0;
-	        int chk = 0;
-	        int count=0;
-	        if(arr.length==1) return 0;
-	        if(arr[0]==0) return -1;
-	        for(int i=0;i<arr.length;i++){
-	            chk=Math.max(arr[i]+i,chk);
-	            if(i==jump){
-	                jump=chk;
-	                count++;
-	                if(chk>=arr.length-1) return count;
-	                if(arr[i]==0) return -1;
-	            }
-	        }
-	        return count;
-	    }
+	 * Given an array Arr[] of N integers. Find the
+	 *  contiguous sub-array(containing at least one number) 
+	 *  which has the maximum sum and return its sum.	 */
+	
+	// KANDANE's Algorithm
+	// arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        // Using Kandane's algorithm
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i =0;i<n;i++){
+            sum+=arr[i];
+            if(max<sum)max=sum;
+            if(sum<0) sum=0;
+        }
+        if(max==0) max=-1;
+        return max;
+    }
 }
