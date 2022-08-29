aa7d2f63496c09b3929e35c731f81695a55e84b1
n-ndini7 <nan11041997dini@gmail.com>
2022-08-29 20:45:02 +0530

    Equilibrium point easy

--- src/com/java/easy/Solution.java
+++ src/com/java/easy/Solution.java
@@ -1,25 +1,25 @@
 package com.java.easy;
 
-import java.util.ArrayList;
-import java.util.TreeSet;
 
-
-/*Given an array of size N-1 such that it only contains distinct 
- * integers in the range of 1 to N. Find the missing element.
+/*Given an array A of n positive numbers. 
+ * The task is to find the first Equilibium Point in the array. 
+	Equilibrium Point in an array is a position such that the sum of elements before it
+ is equal to the sum of elements after it.
  */
 
 public class Solution {
 
-	 int MissingNumber(int array[], int n) {
-	        // Your Code Here
-	        // Using hashing
-	        int[] hsh = new int[n+1];
-	        for(int i =0;i<n-1;i++) 
-	            {hsh[array[i]-1]++;}
-	        for(int i=0;i<hsh.length;i++){
-	            if(hsh[i]==0) return i+1;
-	        }
-	        return -1;
-	    }
+	public static int equilibriumPoint(long arr[], int n) {
+
+        long sum=0;
+        long s2=0;
+        if(n==1) return 1;
+        for(int i=0;i<n;i++)sum+=arr[i];
+        for(int i =0;i<n;i++){
+            s2+=arr[i];
+            if(s2-arr[i]==sum-s2) return i+1;
+        }
+        return -1;
+    }
 	
 }
